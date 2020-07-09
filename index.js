8a2b0838935df666fd8f162b34891962767977c1
Jason Clegg <tjason.clegg@gmail.com>
2020-07-09 13:19:53 -0600

    Ready to deploy

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 // code away!
+require("dotenv").config();
+
 const server = require("./server.js");
 
-server.listen(7000, () => {
-  console.log("\n* Server Running on http://localhost:7000 *\n");
+const port = process.env.PORT || 7000;
+
+server.listen(port, () => {
+  console.log(`\n* Server Running on http://localhost:${port} *\n`);
 });
